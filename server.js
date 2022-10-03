b4a8227b9bba376a3ac68462ae9af2baf30ef1b3
Mehtab Riar <mehtabriar228@hotmail.com>
2022-10-03 13:49:35 -0700

    setup

--- server.js
+++ server.js
@@ -3,16 +3,22 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const app = express();
 app.use(cors());
 
-const PORT = process.env.PORT || 3001;
-
-app.get('/test', (request, response) => {
-
-  response.send('test request received')
-
+mongoose.connect(process.env.DB_URL);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'mongo connection error'));
+db.once('open', function() {
+  console.log('Mongoose is connected to mongo')
 })
 
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
+
+app.get('/test', (request, response) => {
+  response.status(200).send('test request received')
+})
+
