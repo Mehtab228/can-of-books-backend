01eae4d5a3bb0502bf1523f46a3c37fb53c93abf
Mehtab Riar <mehtabriar228@hotmail.com>
2022-10-04 13:16:21 -0700

    finished backend for the lab

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const mongoose = require('mongoose');
 const app = express();
 app.use(cors());
 
+//NEW! dont forget to add
+//If req.body is undefined make sure to use this middleware
+app.use(express.json());
+
 const Book = require('./models/book.js');
 
 mongoose.connect(process.env.DB_URL);
@@ -26,12 +30,51 @@ app.get('/test', (request, response) => {
 
 app.get('/books', getBooks);
 
-async function getBooks(request, response) {
+async function getBooks(request, response, next) {
   try {
     const results = await Book.find();
     response.status(200).send(results);
   } catch (error) {
-    response.status(500).send(error);
+    next(error);
   }
 }
 
+//POST endpoint will trigger a create action on our database
+app.post('/books', postBooks);
+
+async function postBooks(req, res, next) {
+  // double check what gets added to database (for debugging)
+  console.log(req.body);
+  try {
+    // "Book" is the name of the model, .create() is the mongoose method, req.body is the book information
+      const newBook = await Book.create(req.body);
+      res.status(200).send(newBook);
+  } catch (error) {
+      next(error);
+  }
+}
+
+// Delete endpoint
+
+app.delete('/books/:id', deletebook);
+
+async function deletebook(req, res, next) {
+  const id = req.params.id;
+  console.log(id);
+  try {
+     await Book.findByIdAndDelete(id);
+     res.status(204).send('Successfully Deleted') 
+  } catch (error) {
+    next(error)
+  }
+}
+
+app.get('*', (req, res) => {
+  res.status(404).send('Not available: 404 Not Found')
+});
+
+// Put this error handlinig at the bottom
+// it's the last app.use()!
+app.use((error, req, res) => {
+  res.status(500).send(error.message);
+})
